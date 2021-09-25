54b3161c082bf554975a4524aaa95d0ea32245b8
sahidulcse12 <m.sahidulislam.14@gmail.com>
2021-09-25 13:08:46 +0600

    burger project initialize

---
+++ src/Components/BurgerBuilder/Controls/Controls.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Card, CardBody, CardHeader, CardFooter } from 'reactstrap';
+
+
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' }
+]
+
+const BuildControl = props => {
+    return (
+        <div className="d-flex">
+            <div className="ml-5" style={{ fontWeight: 'bold', fontSize: '1.5rem' }}>{props.label}</div>
+            <button className="btn btn-danger btn-sm m-1 ms-auto" onClick={props.removed}>Less</button>
+            <button className="btn btn-success btn-sm m-1" onClick={props.added}>More</button>
+        </div>
+    )
+}
+
+
+const Controls = (props) => {
+    return (
+        <div className="container ml-md-5" style={{ textAlign: 'center' }}>
+            <Card style={{
+                marginTop: "30px",
+                marginBottom: "30px",
+                textAlign: 'center'
+            }}>
+                <CardHeader style={{
+                    backgroundColor: "#D70F64",
+                    color: "white"
+                }}
+                ><h4>Add Ingredients</h4></CardHeader>
+                <CardBody>
+                    {
+                        controls.map(item => {
+                            return <BuildControl
+                                label={item.label}
+                                type={item.type}
+                                key={Math.random()}
+                                added={() => props.ingredientAdded(item.type)}
+                                removed={() => props.ingredientRemoved(item.type)}
+                            />
+                        })
+                    }
+                </CardBody>
+                <CardFooter><h5>Price: <strong>{props.price}</strong> BDT</h5></CardFooter>
+            </Card>
+        </div>
+    );
+};
+
+export default Controls;
